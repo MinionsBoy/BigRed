90ac02ed94bf415576e283c7dc9130982a62deac
MinionsBoy <venom.nfs.ru@gmail.com>
2019-03-25 23:05:43 +0200

    Done

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -4,13 +4,13 @@
     <title>Matrix Admin</title>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="/public/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="/public/css/bootstrap-responsive.min.css" />
-    <link rel="stylesheet" href="/public/css/fullcalendar.css" />
-    <link rel="stylesheet" href="/public/css/matrix-style.css" />
-    <link rel="stylesheet" href="/public/css/matrix-media.css" />
-    <link href="/public/font-awesome/css/font-awesome.css" rel="stylesheet" />
-    <link rel="stylesheet" href="/public/css/jquery.gritter.css" />
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/bootstrap-responsive.min.css" />
+    <link rel="stylesheet" href="css/fullcalendar.css" />
+    <link rel="stylesheet" href="css/matrix-style.css" />
+    <link rel="stylesheet" href="css/matrix-media.css" />
+    <link href="font-awesome/css/font-awesome.css" rel="stylesheet" />
+    <link rel="stylesheet" href="css/jquery.gritter.css" />
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800' rel='stylesheet' type='text/css'>
 </head>
 <body>
@@ -208,18 +208,16 @@
                             <ul>
 
                                 @foreach($post as $posts)
-                                    <li class="clearfix">
-                                        <div class="article-post"> <span class="user-info"> By user № {{$posts->user_id}} / Date: {{date( 'M j, Y H:i', strtotime($posts->created_at)) }} / Time:09:27 AM </span>
+                                    <li>
+                                        <div class="article-post"> <span class="user-info"> By user № {{$posts->user_id}} / Date: {{date( 'M j, Y H:i', strtotime($posts->created_at)) }} </span>
                                             <p><a href="{{route('posts.show', $posts->id)}}">{{$posts->title}}</a> </p>
                                         </div>
-                                        <td>{{substr($posts->description, 0, 15)}}{{ strlen($posts->description)>15 ? "...": ""}}</td>
+                                        <td>{{substr($posts->description, 0, 15)}}{{ strlen($posts->description)>30 ? "...": ""}}</td>
 
                                         <form action="{{url('posts/'.$posts->id)}}" method="POST">
                                             {{csrf_field()}}
                                             {{method_field('DELETE')}}
-
-                                            <button class="btn-mini">
-
+                                            <button class="btn-danger">
                                                 Delete
                                             </button>
                                         </form>

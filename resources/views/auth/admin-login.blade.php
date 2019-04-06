18e19e28a2303a0e2d1a3f41cf0b31a702d25018
MinionsBoy <venom.nfs.ru@gmail.com>
2019-04-06 15:26:20 +0300

    Update

--- resources/views/auth/admin-login.blade.php
+++ resources/views/auth/admin-login.blade.php
@@ -5,6 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
+                    <hr>
                     <div class="card-header">Admin {{ __('Login') }}</div>
 
                     <div class="card-body">
--- resources/views/posts/posts.blade.php
+++ resources/views/posts/posts.blade.php
@@ -5,6 +5,9 @@
              style="background: url(http://commonpixel.com/themes/edgar/images/examples/bg_parallax_05.jpg) no-repeat 50% 50% / cover fixed;">
         <div class="container-fluid projects-fluid">
             <div class="row justify-content-center">
+                <div class="col-sm-4 leftAlign xsAlignReset">
+                    <a href="{{ route('posts.create')}}" class="button primary large">New post</a>
+                </div>
                 @foreach($posts as $post)
                     <div class="col-md-12">
                         <div class="container">
